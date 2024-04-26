2f540991d8148770419049ad1f32695bfbc912b8
mariandrean <m.andreaan@gmail.com>
2024-04-26 23:40:27 +0200

    fix(client/newsform):move-cloudinary-function-to-helpers-folder

--- client/src/pages/NewsForm.jsx
+++ client/src/pages/NewsForm.jsx
@@ -1,49 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createNews, updateNews } from '../services/newsServices';
 import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import axios from 'axios';
+import { uploadImage } from '../helpers/cloudinary';
 
 const NewsForm = ({ method }) => {
   const { handleSubmit, register, setValue } = useForm();
   const navigate = useNavigate();
-  let image;
   const newsId = useParams().id;
-
-  if (method === "update") {
-    const newsData = useLoaderData(newsId);;
-    setValue("news", newsData);
-  }
+  const newsData = useLoaderData(newsId);
+  const [newsImage, setNewsImage] = useState(() => newsData ? newsData.image : "");
+  
+  setValue("news", newsData);
 
   const handleImage = async (e) => {
-    const data = new FormData();
-    data.append("file", e.target.files[0]);
-    data.append("upload_preset", "new_value_preset");
-    const response = await axios.post("https://api.cloudinary.com/v1_1/dgi5ujtli/image/upload", data);
-    image = response.data.secure_url;
+    const response = await uploadImage(e);
+    setNewsImage(response);
   }
 
   const handleNews = async (formData) => {
-    if (image) {
-      formData.news.image = image;
-    }
-    if (method === "create") {
-      formData.news.user_id = 1;
-      await createNews(formData.news);
-    }
-    else if (method === "update") {
+    formData.news.image = newsImage;
+    
+    if (newsData) {
       const response = await updateNews(newsId, formData.news);
       console.log(response)
     }
+    else {
+      formData.news.user_id = 1;
+      await createNews(formData.news);
+    }
+    
     return navigate("/dashboard")
   }
 
   return (
-    <form onSubmit={handleSubmit(formData => handleNews(formData))}>
+    <form onSubmit={handleSubmit(handleNews)}>
       <input {...register("news.title")} id="title" type="text" placeholder='Título' />
       <input {...register("news.subtitle")} id="subtitle" type="text" placeholder='Subtítulo' />
 
       <input {...register("news.image")} id='image' type="file" accept="image/*" onChange={handleImage} />
+      <img src={newsImage} className="w-[250px]" />
 
       <input {...register("news.date")} id="date" type='date' />
       <input {...register("news.content")} id='content' type="text" placeholder='Contenido' />
