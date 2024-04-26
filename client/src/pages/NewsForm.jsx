0035a0720eb2d0ad385ecd03f23d8dce2975431b
mariandrean <m.andreaan@gmail.com>
2024-04-26 18:02:17 +0200

    feat(client/newsform):connect-file-input-with-cloudinary

--- client/src/pages/NewsForm.jsx
+++ client/src/pages/NewsForm.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { createNews, updateNews } from '../services/newsServices';
 import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 const NewsForm = ({ method }) => {
   const { handleSubmit, register, setValue } = useForm();
   const navigate = useNavigate();
+  let image;
   const newsId = useParams().id;
 
   if (method === "update") {
@@ -13,7 +15,18 @@ const NewsForm = ({ method }) => {
     setValue("news", newsData);
   }
 
+  const handleImage = async (e) => {
+    const data = new FormData();
+    data.append("file", e.target.files[0]);
+    data.append("upload_preset", "new_value_preset");
+    const response = await axios.post("https://api.cloudinary.com/v1_1/dgi5ujtli/image/upload", data);
+    image = response.data.secure_url;
+  }
+
   const handleNews = async (formData) => {
+    if (image) {
+      formData.news.image = image;
+    }
     if (method === "create") {
       formData.news.user_id = 1;
       await createNews(formData.news);
@@ -29,7 +42,9 @@ const NewsForm = ({ method }) => {
     <form onSubmit={handleSubmit(formData => handleNews(formData))}>
       <input {...register("news.title")} id="title" type="text" placeholder='Título' />
       <input {...register("news.subtitle")} id="subtitle" type="text" placeholder='Subtítulo' />
-      <input {...register("news.image")} id='image' type="text"/>
+
+      <input {...register("news.image")} id='image' type="file" accept="image/*" onChange={handleImage} />
+
       <input {...register("news.date")} id="date" type='date' />
       <input {...register("news.content")} id='content' type="text" placeholder='Contenido' />
 
