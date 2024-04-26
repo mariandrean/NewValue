b4131484c3c6406d6fc2e1e7bc7170f347c76758
krisneiras <kristygns21@gmail.com>
2024-04-26 13:45:20 +0200

    feat(routes):add-newsValidation

--- server/routes/NewsRouter.ts
+++ server/routes/NewsRouter.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { getAllNews, createNews, deleteNews, updateNews, getNewsById } from "../controllers/NewsController";
+import { newsValidation } from "../validators/FormsValidation";
 
 const router = express.Router();
 
 router.get('/', getAllNews);
-router.post('/', createNews);
+router.post('/', newsValidation,createNews);
 router.delete('/:id', deleteNews);
-router.put('/:id', updateNews);
+router.put('/:id', newsValidation,updateNews);
 router.get('/:id', getNewsById);
 
 export default router;
