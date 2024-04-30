ca23be9812f596af9818b5ef3449016730059018
mariandrean <m.andreaan@gmail.com>
2024-04-30 11:07:10 +0200

    feat(client):add-config-file-in-client

---
+++ client/config.js
@@ -0,0 +1,3 @@
+export const CLOUD_NAME = "dgi5ujtli";
+export const RESOURCE_TYPE = "image";
+export const PRESET_NAME = "new_value_preset";
--- client/src/helpers/cloudinary.js
+++ client/src/helpers/cloudinary.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { CLOUD_NAME, PRESET_NAME, RESOURCE_TYPE } from "../../config";
 
 export const uploadImage = async (e) => {
     const data = new FormData();
     data.append("file", e.target.files[0]);
-    data.append("upload_preset", "new_value_preset");
-    const response = await axios.post("https://api.cloudinary.com/v1_1/dgi5ujtli/image/upload", data);
+    data.append("upload_preset", PRESET_NAME);
+    const response = await axios.post(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/${RESOURCE_TYPE}/upload`, data);
     return response.data.secure_url;
 }
