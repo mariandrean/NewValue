de70e8f0bff69203bd189664da35471f73824be5
mariandrean <m.andreaan@gmail.com>
2024-04-30 15:37:42 +0200

    feat(client/newsform):add-category-checkboxs-to-newsform-and-change-content-input-to-textarea

--- client/src/pages/NewsForm.jsx
+++ client/src/pages/NewsForm.jsx
@@ -16,6 +16,7 @@ const NewsForm = ({ method }) => {
     setValue("subtitle", newsData.subtitle);
     setValue("date", newsData.date);
     setValue("content", newsData.content);
+    setValue("category", newsData.category.split(","))
   }
   const handleImage = async (e) => {
     const response = await uploadImage(e);
@@ -24,12 +25,11 @@ const NewsForm = ({ method }) => {
 
   const onSubmit = async (formData) => {
     formData.image = newsImage;
+    formData.category = formData.category.toString();
 
-    if (newsData) {
-      const response = await updateNews(newsId, formData);
-      console.log(response)
-    }
-    else {
+    if (newsData && method === "update") {
+      await updateNews(newsId, formData);
+    } else if (method === "create") {
       formData.news.user_id = 1;
       await createNews(formData.news);
     }
@@ -39,21 +39,33 @@ const NewsForm = ({ method }) => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("title", { maxLength: { value: 255 } , required: true })} id="title" type="text" placeholder='Título' />
+      <input {...register("title", { maxLength: { value: 255 }, required: true })} id="title" type="text" placeholder='Título' />
       {errors.title && errors.title.type === "required" && <div className="text-red-500">El título es requerido</div>}
       {errors.title && errors.title.type === "maxLength" && <div className="text-red-500">El título debe tener menos de 255 caracteres</div>}
 
       <input {...register("subtitle", { maxLength: { value: 1024 } })} id="subtitle" type="text" placeholder='Subtítulo' />
       {errors.subtitle && errors.subtitle.type === "maxLength" && <div className="text-red-500">El subtítulo debe tener menos de 1024 caracteres</div>}
-
-      <input {...register("image")} id='image' type="file" accept="image/*" onChange={handleImage} />
-      <img src={newsImage} className="w-[250px]" />
+      
+      <fieldset>
+        <input {...register("image")} id='image' type="file" accept="image/*" onChange={handleImage} />
+        <img src={newsImage} className="h-[200px]" />
+      </fieldset>
 
       <input {...register("date", { required: true })} id="date" type='date' />
       {errors.date && errors.date.type === "required" && <div className="text-red-500">La fecha es requerida</div>}
 
-      <input {...register("content", { required: true })} id='content' type="textarea" placeholder='Contenido' />
+      <textarea {...register("content", { required: true })} id='content' placeholder='Contenido' />
       {errors.content && errors.content.type === "required" && <div className="text-red-500">El contenido es requerido</div>}
+
+      <fieldset className='flex-column'>
+        <legend>Categorías:</legend>
+        <label><input {...register("category")} type="checkbox" name="category" id="desarrolloProyectos" value="desarrolloProyectos" /> Desarrollo Proyectos </label>
+        <label><input {...register("category")} type="checkbox" name="category" id="oficinaTecnica" value="oficinaTecnica" /> Oficina Técnica </label>
+        <label><input {...register("category")} type="checkbox" name="category" id="aws" value="aws" /> AWS </label>
+        <label><input {...register("category")} type="checkbox" name="category" id="marketing" value="marketing" /> Marketing </label>
+        <label><input {...register("category")} type="checkbox" name="category" id="consultoria" value="consultoria" /> Consultoría </label>
+        <label><input {...register("category")} type="checkbox" name="category" id="voluntariado" value="voluntariado" /> Voluntariado </label>
+      </fieldset>
 
       <button type="submit">{method === "create" ? "Publicar noticia" : "Guardar"}</button>
       <button onClick={() => navigate('/dashboard')}>Descartar</button>
