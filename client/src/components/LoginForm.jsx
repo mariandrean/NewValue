8c425976a66c6a11ec3b8bf04b8a0bd8bdd139b8
RaquelLores <rlorescasalinas@gmail.com>
2024-05-03 09:00:02 +0200

    update-folders-name

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useUserContext } from '../context/UserContext.jsx';
 import { login } from '../services/usersServices.js'
 
@@ -10,8 +10,7 @@ const LoginForm = () => {
     const [emailError, setEmailError] = useState('');
     const [passwordError, setPasswordError] = useState('');
     const navigate = useNavigate();
-    const { userAuth, setUserAuth } = useUserContext();
-    const { user, setUser } = useUserContext();
+    const { setUserAuth, setUser } = useUserContext();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -39,40 +38,40 @@ const LoginForm = () => {
 
     return (
         <>
-        <div className="flex flex-col items-center justify-center min-h-screen bg-white-100">
-            <form className="space-y-6" action="/login" method="post" onSubmit={handleSubmit}> 
-            {/* ruta para manejar el form: He puesto /login pero hay que ver cual es el en */}
-            <h3 class="text-4xl text-gray-900 mb-20 text-center font-semibold">Login</h3>
-            <h4 class="text-1xl text-gray-900 mb-10 text-center">Acceso al cuadro de mandos</h4>
-                <div>
-                    <label htmlFor="email">
-                        <input className="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600" type="email" value={email} onChange={(e) => {
-                            setEmail(e.target.value);
-                            setEmailError('');
-                        }} required id="email" placeholder="hola@newvalue.es" />
-                        {emailError && <p>{emailError}</p>}
-                        
-                    </label>
-                </div>
-
-                <div>
-                    <label htmlFor="password">
-                        <input className="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600 mb-5" type="password" value={password} onChange={(e) => {
-                            setPassword(e.target.value);
-                            setPasswordError('');}} required id="password" placeholder="********"/>
-                            {passwordError && <p>{passwordError}</p>}
-                    </label>
-                </div>
-
-                <div>
-                    <button className="bg-teal-500 text-white border-green-900 rounded-lg font-semibold py-2 px-4 hover:bg-teal-800 transition duration-300 ease-in-out mb-10">
-                        Acceder
-                    </button>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-white-100">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <h3 class="text-4xl text-gray-900 mb-20 text-center font-semibold">Login</h3>
+                    <h4 class="text-1xl text-gray-900 mb-10 text-center">Acceso al cuadro de mandos</h4>
                     <div>
-                        <p>¿Olvidaste tu contraseña?</p>
+                        <label htmlFor="email">
+                            <input className="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600" type="email" value={email} onChange={(e) => {
+                                setEmail(e.target.value);
+                                setEmailError('');
+                            }} required id="email" placeholder="hola@newvalue.es" />
+                            {emailError && <p>{emailError}</p>}
+
+                        </label>
                     </div>
-                </div>
-            </form>
+
+                    <div>
+                        <label htmlFor="password">
+                            <input className="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600 mb-5" type="password" value={password} onChange={(e) => {
+                                setPassword(e.target.value);
+                                setPasswordError('');
+                            }} required id="password" placeholder="********" />
+                            {passwordError && <p>{passwordError}</p>}
+                        </label>
+                    </div>
+
+                    <div>
+                        <button className="bg-teal-500 text-white border-green-900 rounded-lg font-semibold py-2 px-4 hover:bg-teal-800 transition duration-300 ease-in-out mb-10">
+                            Acceder
+                        </button>
+                        <div>
+                            <p>¿Olvidaste tu contraseña?</p>
+                        </div>
+                    </div>
+                </form>
             </div>
         </>
     );
