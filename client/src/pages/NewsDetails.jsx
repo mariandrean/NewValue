6da7d488db26e56019b4ec41badf56372cae3c46
mariandrean <m.andreaan@gmail.com>
2024-05-06 12:26:19 +0200

    fix(client):add-poppins-and-open-sans-font

--- client/src/pages/NewsDetails.jsx
+++ client/src/pages/NewsDetails.jsx
@@ -5,18 +5,17 @@ const NewsDetails = () => {
   const news = useLoaderData();
 
   return (
-    <>
-      <h1>ACTUALIDAD</h1>
-      <section className='grid grid-cols-5 gap-5 place-content-center place-items-center'>
-        <img src={news.image} alt={news.title} className='col-span-2 object-cover' />
-        <section className='col-span-3 place-self-start'>
-          <h3>{news.title}</h3>
-          <h4>{news.subtitle}</h4>
+    <main className='px-20 py-10'>
+      <h1 className="font-semibold text-lg mb-5">ACTUALIDAD</h1>
+      <section className='grid grid-cols-9 gap-12 place-content-center place-items-center'>
+        <img src={news.image} alt={news.title} className='col-span-4 object-cover place-self-start h-full max-h-[700px] mt-2' />
+        <section className='col-span-5 place-self-start'>
+          <h3 className='font-semibold text-lg'>{news.title}</h3>
+          <h4 className='mb-5'>{news.subtitle}</h4>
           <p>{news.content}</p>
         </section>
       </section>
-
-    </>
+    </main>
   )
 }
 
