fc201cd56e09f0c1ec0df0d059023fdbe862664c
Leite <leite.way@gmail.com>
2024-05-06 12:33:44 +0200

    feat(server/test):complete-CRUD-methods-all-tests-pass

--- server/__test__/helpers/test-helpers.ts
+++ server/__test__/helpers/test-helpers.ts
@@ -28,15 +28,19 @@ export const updatedTestUser = {
 const actualDate = moment().format('YYYY-MM-DD');
 export const testNew = {
     "title": "testTitle",
+    "subtitle": "testSubtitle",
+    "content": "testContent",
+    "category": "testCategory",
     "date": actualDate,
-    "description": "descripcionTest",
     "image": "http://www.imagen.com"
 }
 
 export const updatedTestNew = {
         "title": "updated testTitle",
+        "subtitle": "updated testSubtitle",
+        "content": "updated testContent",
+        "category": "updated testCategory",
         "date": '2000-01-01',
-        "description": "updated descripcionTest",
         "image": "http://www.imagen.com"
 }
 
