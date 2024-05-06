665f9bc073ec0805fdeef013b7e7639856f88b41
RaquelLores <rlorescasalinas@gmail.com>
2024-05-06 10:00:45 +0200

    fix(client):style-register-form

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -16,34 +16,34 @@ const RegisterForm = () => {
     }
     return (
         <>
-            <div className="flex flex-col items-center justify-center min-h-screen bg-white-100">
-                <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col items-center justify-center min-h-screen min-w-[500px] bg-white-100">
+                <form className="min-w-[300px] gap-6 flex flex-col justify-center"onSubmit={handleSubmit(onSubmit)}>
                     <h1 className="text-4xl text-gray-900 mb-20 text-center font-semibold">Register</h1>
                     <h3 className="text-1xl text-gray-900 mb-10 text-center">Crear nuevos usuarios</h3>
-                    <div>
-                        <label>Nombre</label>
-                        <input type="text" {...register('name')} />
+                    <div className="flex">
+                        <label className="mr-7">Nombre</label>
+                        <input className="input border border-gray-400 appearance-none rounded w-full p-3 focus focus:border-teal-500 focus:outline-none active:outline-none active:border-teal-500" type="text" {...register('name')} />
 
                     </div>
-                    <div>
-                        <label>Apellido</label>
-                        <input type="text" {...register('lastname')} />
+                    <div className="flex">
+                        <label className="mr-7">Apellido</label>
+                        <input className="input border border-gray-400 appearance-none rounded w-full p-3 focus focus:border-teal-500 focus:outline-none active:outline-none active:border-teal-500" type="text" {...register('lastname')} />
                     </div>
-                    <div>
-                        <label>Email</label>
-                        <input className="input border border-gray-400 appearance-none rounded w-full px-3 py-3 pt-5 pb-2 focus focus:border-indigo-600 focus:outline-none active:outline-none active:border-indigo-600" type="text" {...register('email', { pattern: /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i })} />
+                    <div className="flex">
+                        <label className="mr-12">Email</label>
+                        <input className="input border border-gray-400 appearance-none rounded w-full p-3 focus focus:border-teal-500 focus:outline-none active:outline-none active:border-teal-500" type="text" {...register('email', { pattern: /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i })} />
                         {errors.email?.type === 'pattern' && <p>El formato del email es incorrecto</p>}
                     </div>
-                    <div>
-                        <label>Contraseña</label>
-                        <input type="text" {...register('password', { required: true })} />
+                    <div className="flex">
+                        <label className="mr-2">Contraseña</label>
+                        <input className="input border border-gray-400 appearance-none rounded w-full p-3 focus focus:border-teal-500 focus:outline-none active:outline-none active:border-teal-500" type="text" {...register('password', { required: true })} />
                         {errors.password?.type === 'required' && <p>El campo password es requerido</p>}
                     </div>
                     {/* <div>
                         <label>Confirmar</label>
                         <input type="text" />
                     </div> */}
-                    <button className="bg-teal-500 text-white border-green-900 rounded-lg font-semibold py-2 px-4 hover:bg-teal-800 transition duration-300 ease-in-out mb-10" type="submit">
+                    <button className="w-[150px] self-center bg-teal-500 text-white border-green-900 rounded-lg font-semibold py-2 px-4 hover:bg-teal-800 transition duration-300 ease-in-out" type="submit">
                         Enviar
                     </button>
                 </form>
