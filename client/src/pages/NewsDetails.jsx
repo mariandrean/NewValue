d309206f13e5f43eb6a1a48c601f2a722977f634
Leite <leite.way@gmail.com>
2024-05-09 13:36:16 +0200

    feat(client):add-share-on-linkedin-button-WIP

--- client/src/pages/NewsDetails.jsx
+++ client/src/pages/NewsDetails.jsx
@@ -14,6 +14,7 @@ const NewsDetails = () => {
           <h3 className='font-semibold text-lg'>{news.title}</h3>
           <h4 className='mb-5'>{news.subtitle}</h4>
           <p>{parse(news.content)}</p>
+          <a href="https://www.linkedin.com/feed/?linkOrigin=LI_BADGE&shareActive=true&shareUrl=https%3A%2F%2Fdev.to%2Fmatteosant_dev%2Fnew-web-portfolio-16bit-os-style-20h9%3Fref%3Ddailydev">Compartir en LinkedIn</a>
         </div>
       </section>
     </>
