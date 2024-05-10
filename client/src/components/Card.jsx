ca620c51629c04bdcf78fa7e87165efd2b633b7e
mariandrean <m.andreaan@gmail.com>
2024-05-10 09:41:14 +0200

    fix(client):fix-card-and-home

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-export const Card = ({news, index}) => {
+export const Card = ({ news, index }) => {
   return (
-    <article className='flex flex-col gap-3'>
+    <Link to={`/news/${news.id}`} className='flex flex-col gap-3' >
       <img src={news.image} alt={news.title} />
-      <h3 className={'font-semibold '+ (index == 0 ? "text-xl" : "text-sm")}>{news.title}</h3>
+      <h3 className={'font-semibold ' + (index == 0 ? "text-xl" : "text-sm")}>{news.title}</h3>
       <h4 className={(index > 0) ? "hidden" : undefined}>{news.subtitle}</h4>
       <p className='text-xs'>{news.date}</p>
-    </article>
+    </Link>
   )
 }
 export default Card;
