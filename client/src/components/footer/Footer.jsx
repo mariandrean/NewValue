5e4b960e72b68862942240da36e1d2a7ba346a64
LuceroHernandez <lu.hernandez726@gmail.com>
2024-05-13 12:40:48 +0200

    feat(client):Add styles footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,31 +1,30 @@
 import React from 'react'
 import Linkedin from '../../../public/assets/icon-linkedin.svg'
 import Instagram from '../../../public/assets/icon-instagram.svg'
+import "../footer/Footer.css"
 
 
 const Footer = () => {
   return (
     <div>
         <footer>
-            <div>
+            <div className="footer">
             <span className="privacidad">
-                <a href="https://newvalue.es/politica-privacidad" className="">Política de cookies</a>
-                <a href="https://newvalue.es/politica-privacidad">Aviso legal</a>
+                <a href="https://newvalue.es/politica-privacidad" className="politica">política de cookies </a>
+                <a href="https://newvalue.es/politica-privacidad" className="aviso"> aviso legal</a>
             </span>
             </div>
             <div className="footer-container2">
             <span className="footer-text2 Content">
-                <a href="mailto:info@newvalue.es?subject=" className="footer-link">info@newvalue.es</a>
+                <a href="mailto:info@newvalue.es?subject=" className="footer-link">info@newvalue.es |</a>
                 <a href="tel:+34611135293" className="footer-link1">+34 611 135 293</a>
             </span>
             </div>
-            <div>
-            <span className="linkedin">
+            <div className="logosFooter">
                 <a href="https://www.linkedin.com/company/new-value-generation/">
                     <img src={Linkedin} alt="icon-linkedin" className="icon-linkedin"/> </a>
                 <a href="https://www.instagram.com/newvaluegeneration/">
                     <img src={Instagram} alt="icon-instagram" className="icon-instagram"/> </a> 
-            </span>
             </div>
             </footer>
     </div>
