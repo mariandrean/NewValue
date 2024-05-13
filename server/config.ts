3e624cf5ddb1e1abac04c01ef9ffcf4bb0ea7d6f
mariandrean <m.andreaan@gmail.com>
2024-05-13 09:23:03 +0200

    fix(server/connection):add-host-variable-(DB_HOST)-in-connection_db-and-config

--- server/config.ts
+++ server/config.ts
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const DB_DEV_NAME = <string> process.env.DB_DEV_NAME;
+export const DB_HOST= <string> process.env.DB_HOST;
 export const DB_USER= <string> process.env.DB_USER;
 export const DB_PASSWORD= <string> process.env.DB_PASSWORD;
 export const DB_PORT = process.env.DB_PORT || 8000;
