07ef97a74640877a2e876324eae244adbd62b872
RaquelLores <rlorescasalinas@gmail.com>
2024-05-14 12:44:23 +0200

    fix(client):remove-useEffect-from-register-page-delete-icons

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 import { userRegister } from "../services/usersServices.js";
 import Swal from 'sweetalert2';
