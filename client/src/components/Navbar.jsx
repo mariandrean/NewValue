6b017811ceb3ef091c6ce38f11d1bc87f4aa42b3
LuceroHernandez <lu.hernandez726@gmail.com>
2024-05-16 12:10:52 +0200

    feat(client):add-complements-navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,54 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import { useUserContext } from "../context/UserContext.jsx";
+import "./Navbar.css"; // Importa los estilos CSS
 
 const Navbar = () => {
   const { userAuth } = useUserContext();
-  const isMobile = window.innerWidth < 600; 
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
-    <>
-      <nav className="flex justify-between items-center pt-12 pb-5">
-        <div className="flex items-center">
+    <nav className="navbar">
+      {/* Icono de hamburguesa */}
+      <div className="menu-icon-wrapper" onClick={toggleMobileMenu}>
+        <div className={`menu-icon ${isMobileMenuOpen ? 'open' : ''}`}>
+          <div className="menu-icon-line"></div>
+          <div className="menu-icon-line"></div>
+          <div className="menu-icon-line"></div>
+        </div>
+      </div>
+
+      {/* Logo */}
+      <div className="logo">
+        <NavLink to="/">
           <img
-            alt="burger-menu"
-            src= '/src/assets/burger-menu.svg'
-            className="mx-10" 
+            alt="logo"
+            src="/src/assets/newvaluelogogris-titulo.svg"
+            className="w-145 h-35"
           />
-        </div>
+        </NavLink>
+      </div>
 
-        <div className={`flex-grow flex items-center justify-center ${isMobile? 'sm:flex sm:justify-center' : ''}`}>
-          <NavLink to="/">
-            <a>
-              <img
-                alt="logo"
-                src="/src/assets/newvaluelogogris-titulo.svg"
-                className="w-145 h-35"
-              />
-            </a>
-          </NavLink>
-        </div>
+      {/* Menú para escritorio y móvil */}
+      <div className="list">
+      <ul className={`sm:flex ${isMobileMenuOpen ? 'block' : 'hidden'}`}>
+        <li>
+          <NavLink to="https://newvalue.es/index" className="menu-link" onClick={() => setMobileMenuOpen(false)}>Inicio</NavLink>
+        </li>
+        <li>
+          <NavLink to="https://newvalue.es/desarrollo-proyectos" className="menu-link" onClick={() => setMobileMenuOpen(false)}>Desarrollo de Proyectos</NavLink>
+        </li>
+        <li>
+          <NavLink to="https://newvalue.es/consultoria-esg" className="menu-link" onClick={() => setMobileMenuOpen(false)}>Consultoría ESG</NavLink>
+        </li>
+        <li>
+          <NavLink to="https://newvalue.es/por-que-new-value" className="menu-link" onClick={() => setMobileMenuOpen(false)}>¿Por qué New Value?</NavLink>
+        </li>
+        <li>
+          <NavLink to="https://newvalue.es/contactanos" className="menu-link" onClick={() => setMobileMenuOpen(false)}>Contacto</NavLink>
+        </li>
+        <li>
+          {userAuth ? (
+            <NavLink to="/dashboard" className="menu-link" onClick={() => setMobileMenuOpen(false)}>Dashboard</NavLink>
+          ) : (
+            <NavLink to="/login" className="menu-link" onClick={() => setMobileMenuOpen(false)}>Login</NavLink>
+          )}
+        </li>
+      </ul>
 
-        <div className="ml-auto flex items-center mr-10">
-          {!isMobile && (
-            <>
+      <div className="button-contact">
               <NavLink to="https://newvalue.es/contactanos">
                 <button className="mr-10 text-black bg-white border rounded-lg font-semibold py-1 px-5 hover:bg-teal-500 hover:text-white transition duration-300 ease-in-out">Contacto</button>
               </NavLink>
-              {userAuth? (
-                <NavLink to="/dashboard">
-                  <button className="text-black bg-white border rounded-lg font-semibold py-1 px-5 hover:bg-teal-500 hover:text-white transition duration-300 ease-in-out">Dashboard</button>
-                </NavLink>
-              ) : (
-                <NavLink to="/login">
-                  <button className="text-black bg-white border rounded-lg font-semibold py-1 px-5 hover:bg-teal-500 hover:text-white transition duration-300 ease-in-out">Login</button>
-                </NavLink>
-              )}
-            </>
-          )}
-        </div>
-      </nav>
-    </>
+      </div>
+      </div>
+    </nav>
   );
 };
 
