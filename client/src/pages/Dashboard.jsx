03c42a8ada6a3a27618f52642cf99bcf93cfd8e3
Leite <leite.way@gmail.com>
2024-05-16 12:35:03 +0200

    feat(client):update-dashboard-style

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -38,20 +38,26 @@ const Dashboard = () => {
     return (
         <>
 
-            <h3 className="text-2xl text-gray-900">Panel de control</h3>
-            <button type="button" onClick={() => navigate('/dashboard/create')} className="bg-teal-500 text-white border-green-900 rounded-lg font-semibold py-2 px-4 hover:bg-teal-800 transition duration-300 ease-in-out">
-                ➕ Nueva Noticia
-            </button>
-            <h4 className="text-xl">Noticias publicadas</h4>
-            {loadingData && <h3>Cargando</h3>}
+            <h3 className="text-2xl text-gray-900 font-bold flex justify-center items-center">Panel de control</h3>
 
             <section class="text-gray-600 body-font overflow-hidden">
                 <div class="container px-5 py-24 mx-auto">
                     <div class="-my-8 divide-y-2 divide-gray-100">
+
+                        <div className="flex justify-between items-center mb-4">
+                            <div>
+                                <h4 className="text-xl">Noticias publicadas</h4>
+                                {loadingData && <h3>Cargando</h3>}
+                            </div>
+                            <button type="button" onClick={() => navigate('/dashboard/create')} className="bg-teal-500 text-white border-green-900 rounded-lg font-semibold py-2 px-4 hover:bg-teal-800 transition duration-300 ease-in-out ">
+                                ➕ Nueva Noticia
+                            </button>
+                        </div>
+
                         {news.map((newsItem, index) => (
                             <div key={index} className="py-8 flex flex-wrap md:flex-nowrap">
                                 <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                                <img src={newsItem.image} className='h-full w-30 object-cover rounded-lg mb-4 md:mb-0 mr-4' />
+                                    <img src={newsItem.image} className='h-full w-30 object-cover rounded-lg mb-4 md:mb-0 mr-4' />
                                 </div>
                                 <div className="md:flex-grow h-50">
                                     <Link to={`/news/${newsItem.id}`} className='flex'>
@@ -61,7 +67,7 @@ const Dashboard = () => {
                                             <p className="leading-relaxed text-lg mb-4">{newsItem.content.substring(0, 100)}...</p>
                                         </div>
                                     </Link>
-                                    <div>
+                                    <div className='flex gap-2'>
                                         <button type="button" onClick={() => navigate(`/dashboard/update/${newsItem.id}`)} className="bg-teal-500 text-white border-green-900 rounded-lg font-semibold py-2 px-4 hover:bg-teal-800 transition duration-300 ease-in-out">Editar</button>
                                         <button type="button" onClick={() => handleDelete(newsItem.id)} className="bg-red-500 text-white border-red-900 rounded-lg font-semibold py-2 px-4 hover:bg-red-700 transition duration-300 ease-in-out">Eliminar</button>
                                     </div>
