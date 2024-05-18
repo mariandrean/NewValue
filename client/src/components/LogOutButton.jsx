15d3d4ad839f960186647268c6db2a68a8d96f8b
mariandrean <m.andreaan@gmail.com>
2024-05-19 00:33:39 +0200

    fix(client):change-logoutbutton

--- client/src/components/LogOutButton.jsx
+++ client/src/components/LogOutButton.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useUserContext } from '../context/UserContext';
-const token = localStorage.getItem("token");
+
 const LogOutButton = () => {
   const { setUser, setUserAuth, setUserRole } = useUserContext();
   const navigate = useNavigate();
@@ -14,8 +14,7 @@ const LogOutButton = () => {
 
   return (
     <button onClick={handleLogOut} className="flex gap-3 items-center">
-      <p>Salir</p>
-      <img src="/src/assets/logout-icon.png" alt="Logout" className='h-3.5 mb-0.5' />
+      <p>Cerrar Sesión</p>
     </button>
   );
 }
