5a802bed07035fc2eac40cbb7a25000b91fa8fd7
RaquelLores <rlorescasalinas@gmail.com>
2024-05-20 09:58:31 +0200

    client(fix):link-tag-in-dashboard

--- client/src/components/DashboardMenu.jsx
+++ client/src/components/DashboardMenu.jsx
@@ -9,11 +9,11 @@ function DashboardMenu() {
   return (
     <div className="w-full flex justify-between items-center text-sm bg-black text-white py-3 px-5">
       <div className="flex gap-3">
-        <Link href="/dashboard">Panel de Control</Link>
+        <Link to ="/dashboard">Panel de Control</Link>
         {path !== '/' &&
           <>
             <p>|</p>
-            <Link href="/" >Portada</Link>
+            <Link to ="/" >Portada</Link>
           </>
         }
       </div>
