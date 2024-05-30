c12ba0ee4468aa5a4b42e4a0f504d93680440856
mariandrean <m.andreaan@gmail.com>
2024-05-30 13:15:19 +0200

    feat(client):add-helmet-in-newsdetails.jsx

--- client/src/pages/NewsDetails.jsx
+++ client/src/pages/NewsDetails.jsx
@@ -3,37 +3,31 @@ import { useLoaderData } from 'react-router-dom';
 import parse from 'html-react-parser'
 import { dateConverter } from '../helpers/dateConverter';
 import Share from '../components/Share';
+import { Helmet } from 'react-helmet-async';
 
 const NewsDetails = () => {
   const [loadingData, setLoadingData] = useState(true);
-  const [metaAdded, setMetaAdded] = useState(false);
   const news = useLoaderData();
   const categories = news.category.split(",");
-  const head = document.querySelector('head')
 
   useEffect(() => {
     if (news) {
-      if (!metaAdded) {
-        setMetaAdded(true);
-        head.innerHTML +=
-          `
-        <meta property="description" content="${news.title}" />
-        <meta property="og:type" content="article" />
-        <meta property="og:site_name" content="New Value" />
-        <meta property="og:description" content="${news.subtitle}" />
-        <meta property="og:title" content="${news.title}" />
-        <meta property="og:url" content="${window.location.href}" />
-        <meta property="og:image" content="${news.image}" />
-        `
-        console.log(head);
-      }
-
       setLoadingData(false)
     }
   }, [loadingData]);
 
   return (
     <>
+      <Helmet>
+        <title>{news.title}</title>
+        <meta property="description" content={news.title} />
+        <meta property="og:type" content="article" />
+        <meta property="og:site_name" content="New Value" />
+        <meta property="og:description" content={news.subtitle} />
+        <meta property="og:title" content={news.title} />
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:image" content={news.image} />
+      </Helmet>
       <h1 className="font-semibold text-lg mb-5">ACTUALIDAD</h1>
       {loadingData && <h3>Cargando</h3>}
       <section className='flex-col md:grid grid-cols-9 gap-12 place-content-center place-items-center'>
@@ -54,7 +48,6 @@ const NewsDetails = () => {
           <hr />
           <Share description={news.title} url={window.location.href} imageUrl={news.image} />
         </div>
-
       </section>
     </>
   )
