a8c0e0b55058b03ef872547b245bc148cf0b4d72
mariandrean <m.andreaan@gmail.com>
2024-06-06 19:14:43 +0200

    fix(client/card):modify-image-and-text-sizes

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,14 +5,14 @@ import { dateConverter } from '../helpers/dateConverter';
 export const Card = ({ news, index }) => {
   return (
     <Link to={`/news/${news.id}`} className='flex flex-col gap-3 h-full' >
-      <img src={news.image} alt={news.title} className={"object-cover " + (index == 0 ? "h-full" : "aspect-[5/4]")} />
+      <img src={news.image} alt={news.title} className={"object-cover " + (index == 0 ? "aspect-[6/7]" : "aspect-[5/4]")} />
       <div className='flex flex-col gap-1'>
         <h3 className={'font-semibold ' + (index == 0 ? "text-xl" : "text-sm")}>
           {news.title.length < 70 ? news.title : (news.title?.slice(0, 90) + "...")}
         </h3>
         {
-          <h4 className={(index == 0 ? "text-xl" : "hidden")}>
-          {news.subtitle.length < 70 ? news.subtitle : (news.title?.slice(0, 90) + "...")}  
+          <h4 className={(index == 0 ? "text-l" : "text-sm")}>
+          {news.subtitle.length < 70 ? news.subtitle : (news.subtitle?.slice(0, 90) + "...")}  
           </h4>
         }
         <p className={'text-xs'}>{dateConverter(news.date)}</p>
